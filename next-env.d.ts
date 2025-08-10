28b18182673760c401037eaadbadcbdc611e3309
Malenkov Gleb <97745015+Malenk0v@users.noreply.github.com>
2025-08-10 04:42:20 -0700

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited directly. It ensures TypeScript
+// definitions for Next.js are available throughout the project.
